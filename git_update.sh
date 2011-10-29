9d8098eedaff3535d6079944a4820e9c7943bb27
Frank Lanitz <frank@frank.uvena.de>
2011-10-29 23:52:52 +0200

    Remove not used code from git script

--- git_update.sh
+++ git_update.sh
@@ -34,19 +34,6 @@ do
         (cd "$i"; git svn fetch; git gc --quiet) &
     elif [ -d "$i/.git/refs/remotes/origin" ]; then
         (cd "$i"; git fetch; git gc --quiet) &
-    else
-        case "$i" in
-          xgit/icewm-upstream)
-            (cd "$i"; git cvsimport ${cvs_options} \
-              -d:pserver:anonymous@icewm.cvs.sourceforge.net:/cvsroot/icewm \
-              icewm-1.2) &
-            ;;
-          git/jedmodes)
-            (cd "$i"; git cvsimport ${cvs_options} \
-        -d:pserver:anonymous@jedmodes.cvs.sourceforge.net:/cvsroot/jedmodes \
-              mode) &
-            ;;
-        esac
     fi
 done
 
